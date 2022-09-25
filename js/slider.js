eb0e6a5d0e9fe97692c900b8a8e0c0cc7de5cb3f
Halyna Kilbovych <vusochina.galina@gmail.com>
2022-09-25 20:23:03 +0300

    закінчила головну

--- js/slider.js
+++ js/slider.js
@@ -1,26 +1,30 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
+
     direction: 'horizontal',
     loop: true,
-  
-    // If we need pagination
+
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
     },
   
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
   
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
+    breakpoints: { 
+      320: { 
+        slidesPerView: 1,
+      },
+      768: { 
+        slidesPerView: 2,
+      }
     },
-    slidesPerView: 2,
 
-    spaceBetween: 30,
+
+    spaceBetween: 40,
 
     slidesPerGroup: 2,
   });
