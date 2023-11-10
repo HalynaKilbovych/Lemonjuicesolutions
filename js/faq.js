9ecfef47b3aed694832501757ea13eca623190ae
Halyna Kilbovych <108695210+HalynaKilbovych@users.noreply.github.com>
2023-11-10 20:21:34 +0200

    fix

--- js/faq.js
+++ js/faq.js
@@ -1,14 +1,18 @@
-var faq = document.getElementsByClassName("faq__question");
-var i;
-setTimeout(faq, 2000);
-for (i = 0; i < faq.length; i++) {
-    faq[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var body = this.nextElementSibling;
-        if (body.style.display === "block") {
-            body.style.display = "none";
-        } else {
-            body.style.display = "block";
+document.addEventListener("DOMContentLoaded", function() {
+    var faq = document.getElementsByClassName("faq__question");
+    var i;
+
+    setTimeout(function() {
+        for (i = 0; i < faq.length; i++) {
+            faq[i].addEventListener("click", function () {
+                this.classList.toggle("active");
+                var body = this.nextElementSibling;
+                if (body.style.display === "block") {
+                    body.style.display = "none";
+                } else {
+                    body.style.display = "block";
+                }
+            });
         }
-    });
-}
+    }, 2000);
+});
